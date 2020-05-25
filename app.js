7cdbf96c3d34bbefb5a8afe243e6bbe17521e624
mosaadaldeen <mohammadsd97@gmail.com>
2020-05-25 18:52:10 +0200

    initail commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const keys = require('./config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// handlebars middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// set static folder
+app.use(express.static(`${__dirname}/public`));
+
+// index route
+app.get('/', (req, res) => {
+    res.render('index', {
+        stripePublishableKey: keys.stripePublishableKey
+    });
+});
+
+app.post('/charge', (req, res) => {
+    const amount = 2500;
+    stripe.customers.create({
+            email: req.body.stripeEmail,
+            source: req.body.stripeToken
+        })
+        .then(customer => stripe.charges.create({
+            amount,
+            description: 'web development ebook',
+            currency: 'usd',
+            customer: customer.id
+        }))
+        .then(charge => res.render('success'));
+});
+
+const Port = process.env.PORT || 5000;
+
+app.listen(Port, () => {
+    console.log(`Listening to port ${Port}`);
+});
